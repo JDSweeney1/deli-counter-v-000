a0572e72d274011b43ae9e4a18bca8230e0ce861
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-05 22:11:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    new_line = "The ine is currently:"
-    katz_deli.each_with_index {|name, index| new_line << "#{index + 1}. #{name} "}
+    new_line = "The line is currently:"
+    katz_deli.each_with_index {|name, index| new_line << " #{index + 1}. #{name} "}
     puts new_line
     end
 end
