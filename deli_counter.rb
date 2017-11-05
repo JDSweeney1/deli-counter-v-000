2c01e836ea4b5c4a216bce23685c744914f827a9
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-05 21:27:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,9 @@
 katz_deli = []
 
 def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  end
 end
 def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are "
